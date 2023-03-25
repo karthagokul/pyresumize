66bf264acc5710a9dbba8c1dd17eca7426e0ed3e
Gokul Kartha <gokul.kartha@bolgatty.nl>
2023-03-25 09:49:37 +0100

    Fixing the data set

--- src/pyresumize/employment_module.py
+++ src/pyresumize/employment_module.py
@@ -36,7 +36,7 @@ class EmployerStandardEngine(EmployerBaseInterface):
         self.nlp.add_pipe("entity_ruler", source=self.nlp_entity)
 
     def process(self, employment_text):
-        """does nothing"""
+        """Process the Custom Entity EMPLOYER"""
 
         doc = self.nlp(employment_text.lower())
         candidate_employment = []
@@ -45,34 +45,4 @@ class EmployerStandardEngine(EmployerBaseInterface):
                 candidate_employment.append(ent.text.capitalize())
                 # print(ent.text)
                 # print(ent.label_)
-        return candidate_employment
-
-        nlp_text = self.nlp(employment_text)
-        doc = self.nlp(employment_text)
-
-        tokens = [token.text for token in nlp_text if not token.is_stop]
-        employ_input_folder = os.path.join(self.config_folder, "employers")
-        utils = Utilities()
-        employers = utils.generate_keywords_from_csv_files(employ_input_folder)
-        employers = list(map(lambda x: str(x).lower(), employers))  # Normalising the Strings to Lower
-        # print("found %d employers "%len(employers))
-
-        candidate_employment = []
-
-        tokens = [token.text for token in nlp_text if not token.is_stop]
-
-        # Lets look at the companies with single word
-        for token in tokens:
-            token = token.lower()
-            if token in employers:
-                # if token not in candidate_employment:
-                candidate_employment.append(token)
-
-        # for the Combined names  such as Operating Systems
-        for token in nlp_text.noun_chunks:
-            token = token.text.lower().strip()
-            if token in employers:
-                candidate_employment.append(token)
-
-        candidate_employment = set(candidate_employment)
         return candidate_employment
